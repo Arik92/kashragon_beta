75374fb7a98aa1afaa98a2bf39d89c87e5bbbbd9
arik92 <arik92@gmail.com>
2020-05-20 12:15:15 +0300

    work so far

--- models/folder.js
+++ models/folder.js
@@ -4,8 +4,14 @@ let Schema = mongoose.Schema;
 
 let folderSchema = new Schema({
     name: String,
-    training_units: [string]
-    // recipts: [] TODO
+    training_units: [string],
+    creator: { type: Schema.Types.ObjectId, ref:"User", $db : "users" },  // user ref of id.
+    allowed_members: [{ type: Schema.Types.ObjectId, ref:"User", $db : "users" }],
+    unit_frames: [{
+        name: String,
+        recipts: [{ type: Schema.Types.ObjectId, ref:"Recipt", $db : "recipts" }],        
+    }]
+    // reports: [] // future features. might consider relocating this
 });
 
 let Item = mongoose.model("Folder", folderSchema);
--- routes/folder_routes.js
+++ routes/folder_routes.js
@@ -3,22 +3,29 @@ let router = express.Router;
 
 let FOLDER = require('../models/folder');
 
-router.post('new_folder', (req, res) => {
-    // execute recipt after it was signed to transfer folder ownership
-    let new_folder = new FOLDER(req.body); // array of folder id's
+router.post('/new_folder', (req, res) => {
+    let new_folder = new FOLDER({}); // array of folder id's
+    // new_folder.creator = ObjectId(req.body.creator_id);
     new_folder.save((req, res) => {
         // TODO: send a log to log
         res.send('done');
         // next();
-    })
-    // , (req, res) => {
-    //     // let items = req.body.items;
-    //     // this wont do, unlike folder items have quantities. how would that work?
-    //     res.send('done!');
-    // })    
+    })    
  });
 
-router.delete('del_folder', (req, res) => {
+ router.get('/folders/:id', (req, res) => { //NEEDS TESTING
+     // get all folders that the user with id param has access to
+     const userId = req.params.id;
+     FOLDER.find({ allowed_members: userId }, (err, folders) => {
+         if (err) {
+             console.error('error fetching folders for that id');
+         } else {
+             res.send(folders);
+         }
+     });
+ });
+
+router.delete('/del_folder', (req, res) => {
    let del_id = rqe.body.id;
    if (!del_id) {
        res.send('folder id not found')
